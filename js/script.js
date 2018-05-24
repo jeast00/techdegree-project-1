2a77bf38057b2f0d82644fc563faa2a5c35512c6
Jonathan E <jonathan_east@ymail.com>
2018-05-24 13:18:48 -0400

    Removed code from .js file

--- js/script.js
+++ js/script.js
@@ -44,15 +44,7 @@ var quotes = [
 
 ];
 
-function getRandomQuote() {
-  var randomQuote = Math.floor(Math.random() * quotes.length) + 1;
-  return randomQuote;
-}
 
-function printQuote() {
-
-
-}
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
