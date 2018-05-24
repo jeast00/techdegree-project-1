5ff5c7cd3280ff9ba630043034caa8bf8d5d09cc
Jonathan E <jonathan_east@ymail.com>
2018-05-24 12:06:44 -0400

    Added new code to .js file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,57 @@
 
 
-/*Create an object array named quotes that holds 5 quote objects and
+/*
+  Create an object array named quotes that holds 5 quote objects and
   each quote has a quote property, a source property, a citation
   property (optional) and a year property (optional).
 */
 var quotes = [
-  {quote : "", source : "", citation : "", year : },
-  {quote : "", source : "", citation : "", year : },
-  {quote : "", source : "", citation : "", year : },
-  {quote : "", source : "", citation : "", year : },
-  {quote : "", source : "", citation : "", year : }
+
+  {
+    quote : "Alrighty then!",
+    source : "Ace Ventura",
+    citation : "Ace Ventura, Pet Detective",
+    year :
+  },
+
+  {
+    quote : "If you're going through Hell, keep going.",
+    source : "Winston Churchill",
+    citation : "",
+    year :
+  },
+
+  {
+    quote : "Accept the challenges so that you can feel the exhilaration of victory.",
+    source : "George S. Patton",
+    citation : "",
+    year :
+  },
+
+  {
+    quote : "I'll be back.",
+    source : "The Terminator",
+    citation : "Terminator 2: Judgement Day",
+    year : 1991
+  },
+
+  {
+    quote : "It is during our darkest moments that we must focus to see the light.",
+    source : "Aristotle",
+    citation : "",
+    year :
+  }
+
 ];
 
 function getRandomQuote() {
-  return;
+  var randomQuote = Math.floor(Math.random() * quotes.length) + 1;
+  return randomQuote;
+}
+
+function printQuote() {
+
+
 }
 
 // event listener to respond to "Show another quote" button clicks
