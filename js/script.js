7b9cacbe54ff3093457d76354d190032fb5dfbf9
Jonathan E <jonathan_east@ymail.com>
2018-05-23 22:17:32 -0400

    Started to add code into .js file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+
+
+/*Create an object array named quotes that holds 5 quote objects and
+  each quote has a quote property, a source property, a citation
+  property (optional) and a year property (optional).
+*/
+var quotes = [
+  {quote : "", source : "", citation : "", year : },
+  {quote : "", source : "", citation : "", year : },
+  {quote : "", source : "", citation : "", year : },
+  {quote : "", source : "", citation : "", year : },
+  {quote : "", source : "", citation : "", year : }
+];
+
+function getRandomQuote() {
+  return;
+}
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
